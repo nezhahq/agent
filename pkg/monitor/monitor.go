a1459b9a7dd60ecac7914e861f5e68e9abc3a95f
uubulb <uub@kuzu.uk>
2024-06-23 21:55:57 +0800

    Add write lock

--- pkg/monitor/monitor.go
+++ pkg/monitor/monitor.go
@@ -7,6 +7,7 @@ import (
 	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"sync/atomic"
 	"syscall"
 	"time"
@@ -54,6 +55,7 @@ var deviceDataFetchAttempts = map[string]int{
 var (
 	updateGPUStatus  int32
 	updateTempStatus int32
+	tempWriteLock    sync.RWMutex
 )
 
 // GetHost 获取主机硬件信息
@@ -216,7 +218,7 @@ func GetState(agentConfig *model.AgentConfig, skipConnectionCount bool, skipProc
 		}
 	}
 
-	go updateTemplatureStat(&temperatureStat)
+	go updateTemperatureStat()
 	ret.Temperatures = temperatureStat
 
 	go updateGPUStat(agentConfig, &gpuStat)
@@ -330,12 +332,15 @@ func updateGPUStat(agentConfig *model.AgentConfig, gpuStat *uint64) {
 	}
 }
 
-func updateTemplatureStat(tempStat *[]model.SensorTemperature) {
-	if !atomic.CompareAndSwapInt32(&updateTempStatus, 0, 1) {
+func updateTemperatureStat() {
+	if !atomic.CompareAndSwapInt32(&updateGPUStatus, 0, 1) {
 		return
 	}
-	defer atomic.StoreInt32(&updateTempStatus, 0)
-	*tempStat = []model.SensorTemperature{}
+	defer atomic.StoreInt32(&updateGPUStatus, 0)
+
+	tempWriteLock.Lock()
+	defer tempWriteLock.Unlock()
+
 	if deviceDataFetchAttempts["Temperatures"] <= maxDeviceDataFetchAttempts {
 		temperatures, err := host.SensorsTemperatures()
 		if err != nil {
@@ -343,14 +348,17 @@ func updateTemplatureStat(tempStat *[]model.SensorTemperature) {
 			println("host.SensorsTemperatures error:", err, "attempt:", deviceDataFetchAttempts["Temperatures"])
 		} else {
 			deviceDataFetchAttempts["Temperatures"] = 0
+			tempStat := []model.SensorTemperature{}
 			for _, t := range temperatures {
 				if t.Temperature > 0 {
-					*tempStat = append(*tempStat, model.SensorTemperature{
+					tempStat = append(tempStat, model.SensorTemperature{
 						Name:        t.SensorKey,
 						Temperature: t.Temperature,
 					})
 				}
 			}
+
+			temperatureStat = tempStat
 		}
 	}
 }
