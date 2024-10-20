319a4b53a366c213ce2a67dd2f0cdc22bbec8fc9
naiba <hi@nai.ba>
2024-10-20 22:54:48 +0800

    refactor cmd arch & auth func

--- model/auth.go
+++ model/auth.go
@@ -6,10 +6,11 @@ import (
 
 type AuthHandler struct {
 	ClientSecret string
+	ClientUUID   string
 }
 
 func (a *AuthHandler) GetRequestMetadata(ctx context.Context, uri ...string) (map[string]string, error) {
-	return map[string]string{"client_secret": a.ClientSecret}, nil
+	return map[string]string{"client_secret": a.ClientSecret, "client_uuid": a.ClientUUID}, nil
 }
 
 func (a *AuthHandler) RequireTransportSecurity() bool {
