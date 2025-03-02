66da118760542039603b6e7c469266747eaee63f
UUBulb <35923940+uubulb@users.noreply.github.com>
2025-03-02 10:43:13 +0800

    add timeout check for Recv() (#169)
    * add timeout check for Recv()
    
    * update dependencies
    
    * fix style

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -59,26 +59,26 @@ func (s *ServiceLogger) SetEnable(enable bool) {
 
 func (s *ServiceLogger) Println(v ...interface{}) {
 	if s.enabled {
-		s.logger.Infof("NEZHA@%s>> %v", time.Now().Format("2006-01-02 15:04:05"), fmt.Sprint(v...))
+		s.logger.Infof("NEZHA@%s>> %v", time.Now().Format(time.DateTime), fmt.Sprint(v...))
 	}
 }
 
 func (s *ServiceLogger) Printf(format string, v ...interface{}) {
 	if s.enabled {
-		s.logger.Infof("NEZHA@%s>> "+format, append([]interface{}{time.Now().Format("2006-01-02 15:04:05")}, v...)...)
+		s.logger.Infof("NEZHA@%s>> "+format, append([]interface{}{time.Now().Format(time.DateTime)}, v...)...)
 	}
 }
 
 func (s *ServiceLogger) Error(v ...interface{}) error {
 	if s.enabled {
-		return s.logger.Errorf("NEZHA@%s>> %v", time.Now().Format("2006-01-02 15:04:05"), fmt.Sprint(v...))
+		return s.logger.Errorf("NEZHA@%s>> %v", time.Now().Format(time.DateTime), fmt.Sprint(v...))
 	}
 	return nil
 }
 
 func (s *ServiceLogger) Errorf(format string, v ...interface{}) error {
 	if s.enabled {
-		return s.logger.Errorf("NEZHA@%s>> "+format, append([]interface{}{time.Now().Format("2006-01-02 15:04:05")}, v...)...)
+		return s.logger.Errorf("NEZHA@%s>> "+format, append([]interface{}{time.Now().Format(time.DateTime)}, v...)...)
 	}
 	return nil
 }
